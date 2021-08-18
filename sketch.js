1dc94716600b9a1573c2e82a52155fd8700fe894
byjusmex4 <88998245+byjusmex4@users.noreply.github.com>
2021-08-18 11:04:00 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var trex ,trex_running;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crear sprite del t-rex.
+ 
+}
+
+function draw(){
+  background("white")
+  
+
+}
